5712c885e37f411aa10788f086ad96f8df84e48d
milton.velillac <milton.velillac@gmail.com>
2026-02-09 15:55:17 -0500

    add the first components

--- quote-duel/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.quoteguess.app',
-  appName: 'QuoteDuel',
-  webDir: 'www'
-};
-
-export default config;
