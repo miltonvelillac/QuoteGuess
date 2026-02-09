20b733a7269dcc764016e3f58b7290417b7a23b8
milton.velillac <milton.velillac@gmail.com>
2026-02-09 15:39:34 -0500

    change the app name

---
+++ quote-duel/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.quoteguess.app',
+  appName: 'QuoteDuel',
+  webDir: 'www'
+};
+
+export default config;
